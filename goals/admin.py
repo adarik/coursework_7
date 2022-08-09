e202c5fcbb4b06e6a425e8cae2026bf441a6c014
neony <neonyan15@gmail.com>
2022-08-09 13:35:27 +0300

    36 fix settings

--- goals/admin.py
+++ goals/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from goals.models import GoalCategory, Goal, GoalComment
 
 
-admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(GoalCategory)
 admin.site.register(Goal)
 admin.site.register(GoalComment)
