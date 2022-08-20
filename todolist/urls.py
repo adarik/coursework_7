d4d1185c7dc9801be91ab4ce65daacc597e40995
neony <neonyan15@gmail.com>
2022-08-20 12:58:30 +0300

    38

--- todolist/urls.py
+++ todolist/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path("oauth/", include("social_django.urls", namespace="social")),
     path("core/", include("core.urls")),
     path("goals/", include("goals.urls")),
+    path("bot/", include("bot.urls")),
     re_path(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
